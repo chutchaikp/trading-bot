801dee9dd2ba808ccc2eef15808ea796f2e90df3
chutchai.kp <chutchai.kp@gmail.com>
2025-01-10 02:11:47 +0700

    learn python 101

---
+++ main.py
@@ -0,0 +1,9 @@
+
+from my_medal import *
+
+a = MyMedal()
+result = a.one()
+
+print(result)
+
+print("hello world 788877")
--- my_medal.py
+++ my_medal.py
@@ -1,3 +1,6 @@
+import random
+
+
 class MyMedal:
 	
 	def __init__(self, country = "Thailand", gold = 0, silver = 0, bronze = 0):
@@ -10,8 +13,22 @@ class MyMedal:
 	# 	# format string len
 	# 	return "country: {:15} g: {:3} s: {:3} b: {:3} ".format(self.country, self.gold, self.silver, self.bronze)
 	
-	def __str__(self):		
-		print('xx')
-		return "hello"
-		# // return "country: {:15} g: {:3} s: {:3} b: {:3} ".format(self.country, self.gold, self.silver, self.bronze)
+	# def __str__(self):		
+	# 	print('xx')
+	# 	return "hello"
+	# 	# // return "country: {:15} g: {:3} s: {:3} b: {:3} ".format(self.country, self.gold, self.silver, self.bronze)
+
+	def hello(self):
+		#  print("hello")
+		return 14
+	
+	def one(self):
+		return 111
+
+	# def __str__(self, one, two, tree):
+	# 	self.one = one
+	# 	self.two = two
+	# 	self.tree  = tree
+	# 	random.randint(1, 3)
+
 	
